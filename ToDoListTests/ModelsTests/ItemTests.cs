3b2bc2d8d075d07a5fd91350df8593ca7eb3c291
David Boedigheimer <stealth259@gmail.com>
2020-08-04 15:00:58 -0700

    Add Changes to make initial push for to do list.

---
+++ ToDoListTests/ModelsTests/ItemTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoListTests
+{
+    [TestClass]
+    public class ItemTests
+    {
+
+        // Test methods will go here.
+
+    }
+}
