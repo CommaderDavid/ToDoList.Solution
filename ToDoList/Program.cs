df0c7cfb137fdb4fa70a349fd703de8c14debd14
David Boedigheimer <stealth259@gmail.com>
2020-09-16 15:19:57 -0700

    Add start of mvc in todo list.

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,41 +1,52 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ToDoList
+namespace ToDoList 
 {
-    public class Program
+    public class Program 
     {
-        public static void Main()
+        public static void Main (string[] args) 
         {
-            Console.WriteLine("Welcome to your To Do List!");
+            var host = new WebHostBuilder ()
+                .UseKestrel ()
+                .UseContentRoot (Directory.GetCurrentDirectory ())
+                .UseIISIntegration ()
+                .UseStartup<Startup> ()
+                .Build ();
 
-            Console.WriteLine("Would you like to add or view your List? (Add/View)");
-            string editToDoList = Console.ReadLine().ToLower();
-
-            if (editToDoList == "add")
-            {
-                Console.WriteLine("What do you wish to add?");
-                string description = Console.ReadLine();
-                Item item = new Item(description);
-                Main();
-            }
-            else if (editToDoList == "view")
-            {
-                Console.WriteLine("Your to do list:");
-                List<Item> items = Item.GetAll();
-                foreach (Item toDo in items)
-                {
-                    Console.WriteLine(toDo.Description);
-                }
-                Console.WriteLine("");
-                Main();
-            }
-            else
-            {
-                Console.WriteLine("You don't have anything on your to do list. Try again");
-                Main();
-            }
+            host.Run ();
         }
     }
 }
+// using System;
+// using System.Collections.Generic;
+// using ToDoList.Models;
+
+// namespace ToDoList {
+//     public class Program {
+//         public static void Main () {
+//             Console.WriteLine ("Welcome to your To Do List!");
+
+//             Console.WriteLine ("Would you like to add or view your List? (Add/View)");
+//             string editToDoList = Console.ReadLine ().ToLower ();
+
+//             if (editToDoList == "add") {
+//                 Console.WriteLine ("What do you wish to add?");
+//                 string description = Console.ReadLine ();
+//                 Item item = new Item (description);
+//                 Main ();
+//             } else if (editToDoList == "view") {
+//                 Console.WriteLine ("Your to do list:");
+//                 List<Item> items = Item.GetAll ();
+//                 foreach (Item toDo in items) {
+//                     Console.WriteLine (toDo.Description);
+//                 }
+//                 Console.WriteLine ("");
+//                 Main ();
+//             } else {
+//                 Console.WriteLine ("You don't have anything on your to do list. Try again");
+//                 Main ();
+//             }
+//         }
+//     }
+// }
