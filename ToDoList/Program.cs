027ee660e26ca5e5571637216b14d96aa336c0f9
David Boedigheimer <stealth259@gmail.com>
2020-10-06 15:03:38 -0700

    Add category files from lesson.

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -18,35 +18,3 @@ namespace ToDoList
         }
     }
 }
-// using System;
-// using System.Collections.Generic;
-// using ToDoList.Models;
-
-// namespace ToDoList {
-//     public class Program {
-//         public static void Main () {
-//             Console.WriteLine ("Welcome to your To Do List!");
-
-//             Console.WriteLine ("Would you like to add or view your List? (Add/View)");
-//             string editToDoList = Console.ReadLine ().ToLower ();
-
-//             if (editToDoList == "add") {
-//                 Console.WriteLine ("What do you wish to add?");
-//                 string description = Console.ReadLine ();
-//                 Item item = new Item (description);
-//                 Main ();
-//             } else if (editToDoList == "view") {
-//                 Console.WriteLine ("Your to do list:");
-//                 List<Item> items = Item.GetAll ();
-//                 foreach (Item toDo in items) {
-//                     Console.WriteLine (toDo.Description);
-//                 }
-//                 Console.WriteLine ("");
-//                 Main ();
-//             } else {
-//                 Console.WriteLine ("You don't have anything on your to do list. Try again");
-//                 Main ();
-//             }
-//         }
-//     }
-// }
