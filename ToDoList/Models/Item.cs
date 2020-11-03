1488ec9c6dbad22a6f32e98ca65748360083a018
David Boedigheimer <stealth259@gmail.com>
2020-11-03 15:57:04 -0800

    Add from lesson 3.3.0.08 - .11

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,12 +5,32 @@ namespace ToDoList.Models
     public class Item 
     {
         public string Description { get; set; }
-        public int Id { get; }
+        public int Id { get; set; }
+
+        public Item(string description)
+        {
+            Description = description;
+        }
 
         public Item (string description, int id) 
         {
             Description = description;
             Id = id;
+        }
+
+        public override bool Equals(System.Object otherItem)
+        {
+            if (!(otherItem is Item))
+            {
+                return false;
+            }
+            else
+            {
+                Item newItem = (Item) otherItem;
+                bool idEquality = (this.Id == newItem.Id);
+                bool descriptionEquality = (this.Description == newItem.Description);
+                return (idEquality && descriptionEquality);
+            }
         }
 
         public static List<Item> GetAll () 
@@ -36,8 +56,42 @@ namespace ToDoList.Models
             return allItems;
         }
 
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+
+            // Begin new code
+
+            cmd.CommandText = @"INSERT INTO items (description) VALUES (@ItemDescription);";
+            MySqlParameter param = new MySqlParameter();
+            param.ParameterName = "@ItemDescription";
+            param.Value = this.Description;
+            cmd.Parameters.Add(param);    
+            cmd.ExecuteNonQuery();
+            Id = (int) cmd.LastInsertedId;
+
+            // End new code
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
         public static void ClearAll () 
         {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM items;";
+            cmd.ExecuteNonQuery();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
         }
 
         public static Item Find(int searchId)
--- ToDoListTests/ModelsTests/ItemTests.cs
+++ ToDoListTests/ModelsTests/ItemTests.cs
@@ -1,3 +1,5 @@
+using MySql.Data.MySqlClient;
+using ToDoList;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
 using System.Collections.Generic;
@@ -13,42 +15,47 @@ namespace ToDoListTests
             Item.ClearAll();
         }
 
-        [TestMethod]
-        public void ItemConstructor_CreatesInstanceOfItem_Item()
+        public ItemTests()
         {
-            Item newItem = new Item("test"); //We pass in "test" as an argument here.
-            Assert.AreEqual(typeof(Item), newItem.GetType());
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list_test;";
         }
 
-        [TestMethod]
-        public void GetDescription_ReturnsDescription_String()
-        {
-            // Arrange
-            string description = "Walk the dog.";
-            Item newItem = new Item(description);
+        // [TestMethod]
+        // public void ItemConstructor_CreatesInstanceOfItem_Item()
+        // {
+        //     Item newItem = new Item("test"); //We pass in "test" as an argument here.
+        //     Assert.AreEqual(typeof(Item), newItem.GetType());
+        // }
 
-            // Act
-            string result = newItem.Description;
+        // [TestMethod]
+        // public void GetDescription_ReturnsDescription_String()
+        // {
+        //     // Arrange
+        //     string description = "Walk the dog.";
+        //     Item newItem = new Item(description);
 
-            // Assert
-            Assert.AreEqual(description, result);
-        }
+        //     // Act
+        //     string result = newItem.Description;
 
-        [TestMethod]
-        public void SetDescription_SetDescription_String()
-        {
-            // Arrange
-            string description = "Walk the dog.";
-            Item newItem = new Item(description);
+        //     // Assert
+        //     Assert.AreEqual(description, result);
+        // }
 
-            // Act
-            string updatedDescription = "Do the dishes";
-            newItem.Description = updatedDescription;
-            string result = newItem.Description;
+        // [TestMethod]
+        // public void SetDescription_SetDescription_String()
+        // {
+        //     // Arrange
+        //     string description = "Walk the dog.";
+        //     Item newItem = new Item(description);
 
-            // Assert
-            Assert.AreEqual(updatedDescription, result);
-        }
+        //     // Act
+        //     string updatedDescription = "Do the dishes";
+        //     newItem.Description = updatedDescription;
+        //     string result = newItem.Description;
+
+        //     // Assert
+        //     Assert.AreEqual(updatedDescription, result);
+        // }
 
         [TestMethod]
         public void GetAll_ReturnsEmptyList_ItemList()
@@ -66,48 +73,76 @@ namespace ToDoListTests
         [TestMethod]
         public void GetAll_ReturnsItems_ItemList()
         {
-            // Arrange
+            //Arrange
             string description01 = "Walk the dog";
             string description02 = "Wash the dishes";
             Item newItem1 = new Item(description01);
+            newItem1.Save(); // New code
             Item newItem2 = new Item(description02);
+            newItem2.Save(); // New code
             List<Item> newList = new List<Item> { newItem1, newItem2 };
 
-            // Act
+            //Act
             List<Item> result = Item.GetAll();
 
-            // Assert
+            //Assert
             CollectionAssert.AreEqual(newList, result);
         }
 
         [TestMethod]
-        public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+        public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
         {
-            //Arrange
-            string description = "Walk the dog.";
-            Item newItem = new Item(description);
+            // Arrange, Act
+            Item firstItem = new Item("Mow the lawn");
+            Item secondItem = new Item("Mow the lawn");
 
-            //Act
-            int result = newItem.Id;
-
-            //Assert
-            Assert.AreEqual(1, result);
+            // Assert
+            Assert.AreEqual(firstItem, secondItem);
         }
 
         [TestMethod]
-        public void Find_ReturnsCorrectItem_Item()
+        public void Save_SavesToDatabase_ItemList()
         {
             //Arrange
-            string description01 = "Walk the dog";
-            string description02 = "Wash the dishes";
-            Item newItem1 = new Item(description01);
-            Item newItem2 = new Item(description02);
+            Item testItem = new Item("Mow the lawn", 0);
 
             //Act
-            Item result = Item.Find(2);
+            testItem.Save();
+            List<Item> result = Item.GetAll();
+            List<Item> testList = new List<Item> { testItem };
 
             //Assert
-            Assert.AreEqual(newItem2, result);
+            CollectionAssert.AreEqual(testList, result);
         }
+
+        // [TestMethod]
+        // public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+        // {
+        //     //Arrange
+        //     string description = "Walk the dog.";
+        //     Item newItem = new Item(description);
+
+        //     //Act
+        //     int result = newItem.Id;
+
+        //     //Assert
+        //     Assert.AreEqual(1, result);
+        // }
+
+        // [TestMethod]
+        // public void Find_ReturnsCorrectItem_Item()
+        // {
+        //     //Arrange
+        //     string description01 = "Walk the dog";
+        //     string description02 = "Wash the dishes";
+        //     Item newItem1 = new Item(description01);
+        //     Item newItem2 = new Item(description02);
+
+        //     //Act
+        //     Item result = Item.Find(2);
+
+        //     //Assert
+        //     Assert.AreEqual(newItem2, result);
+        // }
     }
 }
