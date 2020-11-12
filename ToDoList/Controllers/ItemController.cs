538fdcd326cba67e6a8b6cc157a38751e718214b
David Boedigheimer <stealth259@gmail.com>
2020-11-12 14:49:24 -0800

    Add from lessons 3.3.1

--- ToDoList/Controllers/ItemController.cs
+++ ToDoList/Controllers/ItemController.cs
@@ -1,34 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
-    public class ItemsController : Controller
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
+
+    public ItemsController(ToDoListContext db)
     {
-        [HttpGet("/categories/{categoryId}/items/{itemId}")]
-        public ActionResult Show(int categoryId, int itemId)
-        {
-            Item item = Item.Find(itemId);
-            Category category = Category.Find(categoryId);
-            Dictionary<string, object> model = new Dictionary<string, object>();
-            model.Add("item", item);
-            model.Add("category", category);
-            return View(model);
-        }
-
-        [HttpGet("/categories/{categoryId}/items/new")]
-        public ActionResult New(int categoryId)
-        {
-            Category category = Category.Find(categoryId);
-            return View(category);
-        }
-
-        [HttpPost("/items/delete")]
-        public ActionResult DeleteAll()
-        {
-            Item.ClearAll();
-            return View();
-        }
+      _db = db;
     }
+
+    public ActionResult Index()
+    {
+      List<Item> model = _db.Items.ToList();
+      return View(model);
+    }
+  }
 }
