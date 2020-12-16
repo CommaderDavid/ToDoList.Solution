e5b5c1c9cff59fec8ed33a7f93eef5a05847b0c1
David Boedigheimer <stealth259@gmail.com>
2020-12-16 14:11:32 -0800

    Add edit and delete.

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -46,5 +46,34 @@ namespace ToDoList.Controllers
                 .FirstOrDefault(category => category.CategoryId == id);
             return View(thisCategory);
         }
+
+        public ActionResult Edit(int id)
+        {
+            Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            _db.Entry(category).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+            return View(thisCategory);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+            _db.Categories.Remove(thisCategory);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
