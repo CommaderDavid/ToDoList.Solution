6281290929402947dc287d224aaa7c51797d2bb3
David Boedigheimer <stealth259@gmail.com>
2021-01-26 14:42:17 -0800

    Add many to many to category.

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -42,8 +42,10 @@ namespace ToDoList.Controllers
 
         public ActionResult Details(int id)
         {
-            Category thisCategory = _db.Categories.Include(category => category.Items)
-                .FirstOrDefault(category => category.CategoryId == id);
+            Category thisCategory = _db.Categories
+              .Include(category => category.Items)
+                .ThenInclude(join => join.Item)
+              .FirstOrDefault(category => category.CategoryId == id);
             return View(thisCategory);
         }
 
@@ -61,19 +63,19 @@ namespace ToDoList.Controllers
             return RedirectToAction("Index");
         }
 
-        public ActionResult Delete(int id)
-        {
-            Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-            return View(thisCategory);
-        }
+        //         public ActionResult Delete(int id)
+        //         {
+        //             Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+        //             return View(thisCategory);
+        //         }
 
-        [HttpPost, ActionName("Delete")]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-            _db.Categories.Remove(thisCategory);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //         [HttpPost, ActionName("Delete")]
+        //         public ActionResult DeleteConfirmed(int id)
+        //         {
+        //             Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+        //             _db.Categories.Remove(thisCategory);
+        //             _db.SaveChanges();
+        //             return RedirectToAction("Index");
+        //         }
     }
 }
